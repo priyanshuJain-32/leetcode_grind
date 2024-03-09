acde316e3d2d479ae79617992ef8cdb304ead937
priyanshuJain-32 <priyanshu.jain32@gmail.com>
2024-03-09 08:38:47 +0530

    Easy

---
+++ 1051. Height Checker.py
@@ -0,0 +1,7 @@
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        ans = 0
+        for a,b in zip(heights,sorted(heights)):
+            if a!=b:
+                ans+=1
+        return ans
