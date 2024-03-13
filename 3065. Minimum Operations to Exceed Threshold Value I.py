0c6615c41fb3f9bd580c2708a74a9e9a2b81086b
priyanshuJain-32 <priyanshu.jain32@gmail.com>
2024-03-13 09:00:52 +0530

    Easy

---
+++ 3065. Minimum Operations to Exceed Threshold Value I.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        cnt = 0
+        for i in nums:
+            if i<k:
+                cnt +=1
+        return cnt
