b5b55cda5e2ca63f109d43f7cbaaae85ef291ef0
priyanshuJain-32 <priyanshu.jain32@gmail.com>
2024-03-21 16:12:30 +0530

    Easy

---
+++ 1450. Number of Students Doing Homework at a Given Time.py
@@ -0,0 +1,7 @@
+class Solution:
+    def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
+        c = 0
+        for s,e in zip(startTime, endTime):
+            if s<=queryTime<=e:
+                c+=1
+        return c
