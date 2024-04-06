547a19107ff30aa05ca86a637c1fa2031331ef82
priyanshuJain-32 <priyanshu.jain32@gmail.com>
2024-04-06 12:05:06 +0530

    Easy

---
+++ 535. Encode and Decode TinyURL.py
@@ -0,0 +1,18 @@
+class Codec:
+    def __init__(self):
+        self.decoded = defaultdict(str)
+        self.counter = 0
+
+    def encode(self, longUrl: str) -> str:
+        self.decoded[self.counter]=longUrl
+        short = "https://tinyurl.com/{}".format(self.counter)
+        self.counter += 1
+        return short
+        
+    def decode(self, shortUrl: str) -> str:
+        count = int(shortUrl.split("/")[-1])
+        return self.decoded[count]
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(url))
