bc71edce504445a1105d4216ed24b5b43f1d46a9
priyanshuJain-32 <priyanshu.jain32@gmail.com>
2024-04-30 05:21:44 +0530

    Medium

--- 2415. Reverse Odd Levels of Binary Tree.py
+++ 2415. Reverse Odd Levels of Binary Tree.py
@@ -1,20 +1,36 @@
 class Solution:
     def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        self.L = defaultdict(list)
-        self.count = 0
+        # Another Iterative solution
+        # queue = deque()
+        # queue.append(root)
+        # depth = 1
+        # while queue:
+        #     if depth%2==0:
+        #         l = 0
+        #         r = len(queue)-1
+        #         while l<r:
+        #             queue[l].val,queue[r].val = queue[r].val,queue[l].val
+        #             l+=1
+        #             r-=1
+            
+        #     for _ in range(len(queue)):
+        #         node = queue.popleft()
+        #         if node.left:
+        #             queue.append(node.left)
+        #             queue.append(node.right)
+        #     depth+=1
+        # return root
+
+        # My recursive approach
         def traverse(root,depth):
+            if not root:
+                return
             if not root.left:
                 return root
-            if depth%2==1:
-                self.L[depth].append(root.left)
-                self.L[depth].append(root.right)
-                self.count += 2
-            root.left = traverse(root.left,depth+1)
+            if depth%2==0:
+                root.left.val, root.right.val = root.right.val, root.left.val
             root.right = traverse(root.right,depth+1)
+            root.left = traverse(root.left,depth+1)
             return root
-        root = traverse(root,1)
-        for k,v in self.L.items():
-            n = len(v)
-            for i in range(n//2):
-                v[i].val, v[n-i-1].val = v[n-i-1].val, v[i].val
-        return root
+        return traverse(root,0)
+        
