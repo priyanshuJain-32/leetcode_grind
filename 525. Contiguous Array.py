0c01e411a3e48c4cbfd894cc08ac5de9af4391eb
priyanshuJain-32 <priyanshu.jain32@gmail.com>
2024-05-09 12:49:01 +0530

    Revise Improved Medium

--- 525. Contiguous Array.py
+++ 525. Contiguous Array.py
@@ -1,17 +1,11 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
-        total = 0
-        n = len(nums)
-        max_len = 0
+        n,total,maxL = len(nums),0,0
         pref = {0:-1}
-        # pref[0] = -1
-        for i in range(n):
-            if nums[i]:
-                total += 1
-            else:
-                total -= 1
+        for i,n in enumerate(nums):
+            total += n if n else -1
             try:
-                max_len = max(max_len, i-pref[total])
+                maxL = max(maxL, i-pref[total])
             except:
                 pref[total] = i
-        return max_len
+        return maxL
