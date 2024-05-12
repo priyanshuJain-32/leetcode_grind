5f7a992bbdbcbd9e95d560de601ffe2cd9a26ac4
priyanshuJain-32 <priyanshu.jain32@gmail.com>
2024-05-12 07:32:52 +0530

    Easy

--- 2373. Largest Local Values in a Matrix.py
+++ 2373. Largest Local Values in a Matrix.py
@@ -1,21 +1,8 @@
 class Solution:
     def largestLocal(self, grid: List[List[int]]) -> List[List[int]]:
-        n = len(grid)
-        ans = [[0]*(n-2) for _ in range(n-2)]
-        
-        k = 0
-        while k<n-2:
-            for j in range(n):
-                grid[j][k] = max(grid[j][k:k+3])
-            k += 1
-       
-	k = 0
-        while k<n-2:
-            for i in range(n-2):
-                l = []
-                for j in range(k,k+3):
-                    l.append(grid[j][i])
-                
-                ans[k][i] = max(l)
-            k+=1
+        m,n = len(grid),len(grid[0])
+        ans = [[0 for _ in range(n-2)] for _ in range(m-2)]
+        for i in range(1,m-1):
+            for j in range(1,n-1):
+                ans[i-1][j-1] = max(max(grid[i-1][j-1:j+2]),max(grid[i][j-1:j+2]),max(grid[i+1][j-1:j+2]))
         return ans
