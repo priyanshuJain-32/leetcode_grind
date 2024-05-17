c4e2a4102bd2fdce8c18557bf242c48cda45fbaf
priyanshuJain-32 <priyanshu.jain32@gmail.com>
2024-05-17 19:05:22 +0530

    Medium

--- 162. Find Peak Element.py
+++ 162. Find Peak Element.py
@@ -1,5 +1,20 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
+        l = 0
+        r = len(nums)-1
+        while l<r:
+            m = (l+r)//2
+            if nums[m]<nums[m+1]:
+                l = m+1
+            else:
+                r = m
+        return l
+            
+
+
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
         n = len(nums)
         if n==1:
             return 0
